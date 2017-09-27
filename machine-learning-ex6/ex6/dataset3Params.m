e9ec7d0d8e2d723ad95b3d984e26b9afef6cc4e0
dmsun <johnson.michael.david@gmail.com>
2017-09-28 09:33:33 +1000

    updated code for ex6

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,10 +23,23 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+CVec = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigmaVec = [0.01 0.03 0.1 0.3 1 3 10 30];
+error = zeros(8, 8); 
+ for i = 1:length(CVec)
+   C = CVec(i);
+   for j = 1:length(sigmaVec)
+     sigma = sigmaVec(j);
+     model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+     predictions = svmPredict(model, Xval);
+     error(i,j) = mean(double(predictions ~= yval));
+  end
+end
+[minval, Cindex] = min(min(error,[],2));
+[minval, sigmaindex] = min(min(error,[],1));
 
-
-
-
+C=CVec(Cindex);
+sigma=sigmaVec(sigmaindex);
 
 
 % =========================================================================
